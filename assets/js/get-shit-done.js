2e301613ca2e1b281a553980db2a48f3187120a6
Christoffer Mässing <c.massing1992@gmail.com>
2016-01-16 03:38:26 +0100

    About page & Icons
    Lägga till transparant bakgrund och byta iconer samt text till vit

--- assets/js/get-shit-done.js
+++ assets/js/get-shit-done.js
@@ -4,10 +4,10 @@ transparent = true;
 $(document).ready(function(){
     /*      Activate the switches with icons      */
     $('.switch')['bootstrapSwitch']();
-      
+
     /*      Activate regular switches        */
     $("[data-toggle='switch']").wrap('<div class="switch" />').parent().bootstrapSwitch();
-    
+
     $('[data-toggle="search"]').click(function(){
         if(searchVisible == 0){
             searchVisible = 1;
@@ -22,50 +22,50 @@ $(document).ready(function(){
             $('.navbar-search-form').fadeOut(function(){
                 $('.navbar-search-form input').blur();
             });
-        } 
+        }
     });
-    
+
     $('[data-toggle="gsdk-collapse"]').hover(
     function(){
             console.log('on hover');
             var thisdiv = $(this).attr("data-target");
 
             if(!$(this).hasClass('state-open')){
-                $(this).addClass('state-hover');                
+                $(this).addClass('state-hover');
                 $(thisdiv).css({
                     'height':'30px'
-                });    
+                });
             }
-            
+
         },
         function(){
             var thisdiv = $(this).attr("data-target");
             $(this).removeClass('state-hover');
-            
+
             if(!$(this).hasClass('state-open')){
                 $(thisdiv).css({
                     'height':'0px'
-                });     
-            }          
+                });
+            }
         }
     );
-    
+
     $('[data-toggle="gsdk-collapse"]').click(
     function(event){
             event.preventDefault();
-                        
+
             var thisdiv = $(this).attr("data-target");
             var height = $(thisdiv).children('.panel-body').height();
-            
+
             if($(this).hasClass('state-open')){
                 $(thisdiv).css({
                     'height':'0px',
-                }); 
-                $(this).removeClass('state-open');    
+                });
+                $(this).removeClass('state-open');
             } else {
                 $(thisdiv).css({
                     'height':height,
-                }); 
+                });
                 $(this).addClass('state-open');
             }
         }
@@ -77,11 +77,11 @@ $(function () {
         var thisdiv = $(this).attr("data-target");
         $(thisdiv).addClass("gsdk-collapse");
     });
-    
+
 });
 
 $(document).scroll(function() {
-    if( $(this).scrollTop() > 260 ) {
+    if( $(this).scrollTop() > 320 ) {
         if(transparent) {
             transparent = false;
             $('nav[role="navigation"]').removeClass('navbar-transparent');
@@ -93,16 +93,3 @@ $(document).scroll(function() {
         }
     }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
